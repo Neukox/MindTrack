92e695226039614428a9e48fb413b0fe082f0ba6
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-10-22 14:23:33 -0300

    feat: adicionar documentação Swagger

--- backend/src/register/register.controller.ts
+++ backend/src/register/register.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post, Body, HttpStatus, HttpCode } from '@nestjs/common';
 import { RegisterService } from './register.service';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 type RegisterPayload = {
   username: string;
@@ -11,9 +12,27 @@ type RegisterPayload = {
 export class RegisterController {
   constructor(private readonly registerService: RegisterService) {}
 
+  @ApiOperation({
+    summary: 'Registrar um novo usuário',
+    description:
+      'Cria um novo usuário no sistema com as informações fornecidas.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Usuário registrado com sucesso',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Dados de registro inválidos',
+  })
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  async register(@Body() body: RegisterPayload): Promise<{ message: string; user: { id: string; username: string; email: string } }> {
+  async register(
+    @Body() body: RegisterPayload,
+  ): Promise<{
+    message: string;
+    user: { id: string; username: string; email: string };
+  }> {
     const result = await this.registerService.registerUser(body);
     return result;
   }
