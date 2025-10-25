ed4b5b325c0b2c088d31272930265b43287b2983
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-10-25 08:35:24 -0300

    feat(metrics): adicionar service e controller de métricas

--- backend/src/metrics/metrics.module.ts
+++ backend/src/metrics/metrics.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MetricsService } from './metrics.service';
+import { ReflexaoModule } from '@/reflexao/reflexao.module';
+import { MetricsController } from './metrics.controller';
 
-@Module({})
+@Module({
+  imports: [ReflexaoModule],
+  providers: [MetricsService],
+  controllers: [MetricsController],
+  exports: [MetricsService],
+})
 export class MetricsModule {}
